48d92308d475432dab18a6c0b1d02949179b30c3
Ian Pierce <iandpierce@gmail.com>
2018-01-27 19:37:28 -0600

    fixed ticker put error

--- puts.go
+++ puts.go
@@ -13,44 +13,44 @@ func PutBalances(key string, balances exchanges.Balances) {
 	for currency, balance := range balances {
 		err := client.HSet(key, currency, balance).Err()
 		if err != nil {
-			log.Error("error setting balance", "module", "streams", "currency", currency, "balance", balance)
+			log.Error("error setting balance", "module", "streams", "currency", currency, "balance", balance, "error", err)
 		}
 	}
 }
 
 func PutTicker(key string, ticker exchanges.Ticker) {
 	if ticker.LastTrade != nil {
-		err := client.HSet(key, "lastTrade", ticker.LastTrade).Err()
+		err := client.HSet(key, "lastTrade", *ticker.LastTrade).Err()
 		if err != nil {
-			log.Error("error setting last trade for ticker", "module", "streams", "key", key)
+			log.Error("error setting last trade for ticker", "module", "streams", "key", key, "error", err)
 		}
 	}
 
 	if ticker.HighestBid != nil {
-		err := client.HSet(key, "highestBit", ticker.HighestBid).Err()
+		err := client.HSet(key, "highestBit", *ticker.HighestBid).Err()
 		if err != nil {
-			log.Error("error setting highest bid for ticker", "module", "streams", "key", key)
+			log.Error("error setting highest bid for ticker", "module", "streams", "key", key, "error", err)
 		}
 	}
 
 	if ticker.LowestAsk != nil {
-		err := client.HSet(key, "lowestAsk", ticker.LowestAsk).Err()
+		err := client.HSet(key, "lowestAsk", *ticker.LowestAsk).Err()
 		if err != nil {
-			log.Error("error setting lowest ask for ticker", "module", "streams", "key", key)
+			log.Error("error setting lowest ask for ticker", "module", "streams", "key", key, "error", err)
 		}
 	}
 
 	if ticker.TwentyFourHourHigh != nil {
-		err := client.HSet(key, "twentyFourHourHigh", ticker.TwentyFourHourHigh).Err()
+		err := client.HSet(key, "twentyFourHourHigh", *ticker.TwentyFourHourHigh).Err()
 		if err != nil {
-			log.Error("error setting 24 hour high for ticker", "module", "streams", "key", key)
+			log.Error("error setting 24 hour high for ticker", "module", "streams", "key", key, "error", err)
 		}
 	}
 
 	if ticker.TwentyFourHourLow != nil {
-		err := client.HSet(key, "twentyFourHourHigh", ticker.TwentyFourHourLow).Err()
+		err := client.HSet(key, "twentyFourHourHigh", *ticker.TwentyFourHourLow).Err()
 		if err != nil {
-			log.Error("error setting 24 hour low for ticker", "module", "streams", "key", key)
+			log.Error("error setting 24 hour low for ticker", "module", "streams", "key", key, "error", err)
 		}
 	}
 }
@@ -62,14 +62,14 @@ func PutOrderBook(key string, orderBook exchanges.OrderBook) {
 	for price, volume := range orderBook.Asks {
 		err := client.HSet(asksKey, price, volume).Err()
 		if err != nil {
-			log.Error("error setting ask for order book", "module", "streams", "key", key)
+			log.Error("error setting ask for order book", "module", "streams", "key", key, "error", err)
 		}
 	}
 
 	for price, volume := range orderBook.Bids {
 		err := client.HSet(bidsKey, price, volume).Err()
 		if err != nil {
-			log.Error("error setting bid for order book", "module", "streams", "key", key)
+			log.Error("error setting bid for order book", "module", "streams", "key", key, "error", err)
 		}
 	}
 }
